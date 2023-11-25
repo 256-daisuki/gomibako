43534145da426d810f92e862073a16dccfd28630
256-daisuki <256mitdaisuki@gmail.com>
2023-11-26 01:16:14 +0900

    node.jsをちゃんと勉強しようかなと思ってね

---
+++ nodejs/if.js
@@ -0,0 +1,9 @@
+const hoge = 'hello';
+
+if(hoge === 'hello'){
+    console.log('hello world');
+} else if(hoge === 'world'){
+    console.log('world hello');
+} else {
+    console.log('aaaaaaaaaaaaaaa');
+}
