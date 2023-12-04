df98562ed2e817d40f58e8e077ead09faff61b6f
256-daisuki <256mitdaisuki+git@gmail.com>
2023-12-04 16:08:23 +0900

    omikujiを作りました omikuji_answerがバカっぽくて好き

---
+++ joshishougakusei/omikuji/script.js
@@ -0,0 +1,4 @@
+const p_element = document.querySelector("#answer");
+const omikuji = ["凶", "吉", "末吉", "庄吉", "忠吉", "代吉"];
+const omikuji_answer = omikuji[Math.floor(Math.random() * omikuji.length)];
+p_element.textContent = omikuji_answer;
