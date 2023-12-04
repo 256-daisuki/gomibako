2d536f4b060ba6770151b039131796c5b72e7be8
256-daisuki <256mitdaisuki+git@gmail.com>
2023-12-04 16:45:36 +0900

    代吉if追加

--- joshishougakusei/omikuji/script.js
+++ joshishougakusei/omikuji/script.js
@@ -1,6 +1,16 @@
-function btnClick(){
+function btnClick() {
     const p_element = document.querySelector("#answer");
+    const a_element = document.querySelector("#link");
+    const body = document.querySelector("#body")
     const omikuji = ["凶", "吉", "末吉", "庄吉", "忠吉", "代吉"];
     const omikuji_answer = omikuji[Math.floor(Math.random() * omikuji.length)];
     p_element.textContent = omikuji_answer;
+
+    if (omikuji_answer === "代吉") {
+        a_element.style.display = "inline";
+        body.style.backgroundColor = "#ff0000";
+    } else {
+        a_element.style.display = "none";
+        body.style.backgroundColor  = "#fff";
+    }
 }
