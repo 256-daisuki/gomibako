dab743c41e97d71d4103ea477a08054f153dfe36
256-daisuki <256mitdaisuki+git@gmail.com>
2023-12-04 16:20:14 +0900

    ボタンを追加

--- joshishougakusei/omikuji/script.js
+++ joshishougakusei/omikuji/script.js
@@ -1,4 +1,6 @@
-const p_element = document.querySelector("#answer");
-const omikuji = ["凶", "吉", "末吉", "庄吉", "忠吉", "代吉"];
-const omikuji_answer = omikuji[Math.floor(Math.random() * omikuji.length)];
-p_element.textContent = omikuji_answer;
+function btnClick(){
+    const p_element = document.querySelector("#answer");
+    const omikuji = ["凶", "吉", "末吉", "庄吉", "忠吉", "代吉"];
+    const omikuji_answer = omikuji[Math.floor(Math.random() * omikuji.length)];
+    p_element.textContent = omikuji_answer;
+}
